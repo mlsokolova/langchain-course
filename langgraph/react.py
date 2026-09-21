76738c1cd043e6e2fc4d6ca43edb310edfc9dd1b
mlsokolova <mlsokolova@gmail.com>
2026-09-21 22:54:21 +0300

    graph

--- langgraph/react.py
+++ langgraph/react.py
@@ -12,5 +12,5 @@ def triple(num:float) -> float:
 
 tools = [TavilySearch(max_results=1), triple]
 
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
 
